6237beaeadec8c703042a6893ad8aa007e209a8a
Bridget2960 <164809093+Bridget2960@users.noreply.github.com>
2024-04-19 01:20:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
 import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
-
-
-import "./App.js";
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <h1>
-         Weather App
-        </h1> 
-        
-        
-    
-<Weather />
-        
-      </header>
-     <div className="footer">
-     <h2>
-      This project was coded by
-      <a href="https://glowing-narwhal-291ee4.netlify.app/" target="-blank">
-        Noluthando Bridget Siluma
-      </a>
-      and is
-      <a href="https://github.com/Bridget2960" target="-blank" title="GitHub Website"
-        >on GitHub</a
-      >
-      and
-      <a
-        href="https://www.shecodes.io/"
-        target="_blank"
-        title="SheCodes Website"
-        >hosted on SheCodes</a
-      >
-    </h2>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Noluthando Bridget Siluma
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Bridget2960/my-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://glowing-narwhal-291ee4.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
